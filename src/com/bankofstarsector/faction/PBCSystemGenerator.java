750ac2aac24f9384a8f578d86c1256a5e6c41164
TheAlanK <xlanzko@hotmail.com>
2026-03-06 18:47:17 -0300

    Fix system not explored: mark as entered and entities non-discoverable
    Core world systems need setEnteredByPlayer(true) so they appear
    explored on the hyperspace map. All entities (planets, stations,
    relays, nav buoys, sensor arrays, jump points) set to
    setDiscoverable(false) so they're visible without exploration.

--- src/com/bankofstarsector/faction/PBCSystemGenerator.java
+++ src/com/bankofstarsector/faction/PBCSystemGenerator.java
@@ -30,11 +30,11 @@ public class PBCSystemGenerator {
         StarSystemAPI system = sector.createStarSystem("Aurum");
         system.getLocation().set(-4000, 6000);
 
-        // Mark as a core world system - this makes it show on the map like
-        // Corvus, Askonia, etc. instead of an unexplored procgen system
+        // Mark as a core world system - fully explored, like Corvus or Askonia
         system.addTag(Tags.THEME_CORE);
         system.addTag(Tags.THEME_CORE_POPULATED);
         system.setProcgen(false);
+        system.setEnteredByPlayer(true);
         system.setType(StarSystemGenerator.StarSystemType.SINGLE);
 
         // Star
@@ -44,19 +44,23 @@ public class PBCSystemGenerator {
         // Planet 1: Bullion - HQ world (terran)
         PlanetAPI bullion = system.addPlanet("pbc_bullion", star, "Bullion", "terran", 30, 150, 3500, 250);
         bullion.setCustomDescriptionId("pbc_bullion");
+        bullion.setDiscoverable(false);
 
         // Planet 2: Vault - Barren secure world
         PlanetAPI vault = system.addPlanet("pbc_vault", star, "Vault", "barren", 150, 80, 5500, 350);
         vault.setCustomDescriptionId("pbc_vault");
+        vault.setDiscoverable(false);
 
         // Planet 3: Ledger - Gas giant
         PlanetAPI ledger = system.addPlanet("pbc_ledger", star, "Ledger", "gas_giant", 270, 300, 8000, 500);
         ledger.setCustomDescriptionId("pbc_ledger");
+        ledger.setDiscoverable(false);
 
         // Ledger Station (in orbit around gas giant)
         SectorEntityToken ledgerStation = system.addCustomEntity("pbc_ledger_station",
             "Ledger Station", "station_mining00", FACTION_ID);
         ledgerStation.setCircularOrbitPointingDown(ledger, 0, 500, 40);
+        ledgerStation.setDiscoverable(false);
 
         // Ring band decoration
         system.addRingBand(star, "misc", "rings_dust0", 256f, 3, Color.WHITE, 256f, 2000, 120f, null, null);
@@ -65,21 +69,31 @@ public class PBCSystemGenerator {
         SectorEntityToken relay = system.addCustomEntity("pbc_relay", "Aurum Relay",
             "comm_relay", FACTION_ID);
         relay.setCircularOrbitPointingDown(star, 180, 4500, 300);
+        relay.setDiscoverable(false);
 
         // Nav buoy
         SectorEntityToken navBuoy = system.addCustomEntity("pbc_nav_buoy", "Aurum Nav Buoy",
             "nav_buoy", FACTION_ID);
         navBuoy.setCircularOrbitPointingDown(star, 90, 6000, 400);
+        navBuoy.setDiscoverable(false);
 
         // Sensor array
         SectorEntityToken sensor = system.addCustomEntity("pbc_sensor", "Aurum Sensor Array",
             "sensor_array", FACTION_ID);
         sensor.setCircularOrbitPointingDown(star, 270, 6500, 420);
+        sensor.setDiscoverable(false);
 
         // Generate hyperspace jump points
         system.autogenerateHyperspaceJumpPoints(true, true);
 
-        log.info("Bank of Starsector: Aurum system structure generated (core world).");
+        // Mark all jump points as non-discoverable too
+        for (SectorEntityToken entity : system.getAllEntities()) {
+            if (entity.hasTag(Tags.JUMP_POINT)) {
+                entity.setDiscoverable(false);
+            }
+        }
+
+        log.info("Bank of Starsector: Aurum system generated (core world, pre-explored).");
     }
 
     /**
